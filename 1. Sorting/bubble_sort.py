382f07d55fb1cbde133909a1734ff0aa9fb92315
Dhruv <dhruvdev7794@gmail.com>
2018-01-27 14:25:58 -0500

    minor optimization s

--- 1. Sorting/bubble_sort.py
+++ 1. Sorting/bubble_sort.py
@@ -1,7 +1,8 @@
 from random import *
 
 def bubbleSort(arr):
-    while True:
+    j = 0
+    for i in range(0, len(arr) - (1+j)):
         flag = 0
         for i in range(0, len(arr)-1):
             if arr[i]>arr[i+1]:
@@ -9,11 +10,12 @@ def bubbleSort(arr):
                 arr[i] = arr[i+1]
                 arr[i+1] = temp
                 flag = 1
+        j += 1
         if flag == 0:
             print(arr)
             return True
 
 testArray = []
-for i in range(0,5):
+for i in range(0,10):
     testArray.append(randint(1,100))
 bubbleSort(testArray)
